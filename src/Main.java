87b7bd6768c0cecd002fe69901ebe1f956d458ad
erg5995 <erg5995@rit.edu>
2020-10-19 23:04:48 -0400

    Implemented Matrix

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,47 @@
 import function.activation.IActivationFunction;
 import function.error.IErrorFunction;
+import math.Matrix;
 import network.Network;
 import network.Perceptron;
 
 public class Main {
     public static void main(String[] args) {
 
-        Double[] input = {0.6, 2.4};
+        Matrix m = new Matrix(2, 2);
+        m.randomInitialization(-10, 10);
 
-        Network network = new Network(1, 1, 1, true, 1, input, (double in) -> in < 0 ? -1 : 1);
+        Matrix o = new Matrix(2, 2);
+        o.randomInitialization(0, 2);
 
-        Perceptron perceptron = new Perceptron(2,
-                (IActivationFunction) ((double in) -> in < 0 ? -1 : 1),
-                (IErrorFunction) ((double target, double mark) -> target - mark), 1d, 0.1);
+        System.out.println("m:\n" + m);
+        System.out.println("o:\n" + o);
 
+        m.add(o);
+
+        System.out.println("----------------------\n" + m);
+
+
+        Double[][] mData = {
+                {5d, -3d},
+                {-1d, 7d},
+                {-6d, 3d}
+        };
+
+        Double[][] oData = {
+                {-2d, 2d, 1d},
+                {1d, 3d, 2d}
+        };
+
+        m = new Matrix(2, 3, mData);
+        o = new Matrix(3, 2, oData);
+
+        System.out.println("\n\n\n");
+        System.out.println("m:\n" + m);
+        System.out.println("o:\n" + o);
+
+        m.mult(o);
+
+        System.out.println("----------------------\n" + m);
 
 
     }
---
+++ src/math/Matrix.java
@@ -0,0 +1,158 @@
+package math;
+
+import java.text.DecimalFormat;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Matrix {
+
+    int numRows;
+    int numCols;
+
+    Double[][] data;
+
+    public Matrix(int numRows, int numCols) {
+        this.numRows = numRows;
+        this.numCols = numCols;
+
+        this.data = new Double[numCols][numRows];
+
+        for(int col = 0; col < numCols; col++){
+            for(int row = 0; row < numRows; row++) {
+                this.data[row][col] = 0d;
+            }
+        }
+    }
+
+    public Matrix(int numRows, int numCols, Double[][] data) {
+        this.data = data;
+        this.numRows = numRows;
+        this.numCols = numCols;
+    }
+
+    public void randomInitialization(double min, double max) {
+        for(int col = 0; col < numCols; col++){
+            for(int row = 0; row < numRows; row++) {
+                this.data[row][col] = ThreadLocalRandom.current().nextDouble(min, max);
+            }
+        }
+    }
+
+    public void add(Object o) {
+        if(o instanceof Double) {
+            Double od = (Double) o;
+            addScalar(od);
+        }else if(o instanceof Integer) {
+            Integer oi = (Integer) o;
+            addScalar(oi);
+        }else if(o instanceof Float) {
+            Float of = (Float) o;
+            addScalar(of);
+        }else if(o instanceof Matrix) {
+            Matrix om = (Matrix) o;
+            addMatrix(om);
+        }
+    }
+
+    private void addScalar(double o) {
+        for(int col = 0; col < numCols; col++){
+            for(int row = 0; row < numRows; row++) {
+                data[row][col] += o;
+            }
+        }
+    }
+
+    private void addMatrix(Matrix o) {
+        assert o.numRows == this.numRows;
+        assert o.numCols == this.numCols;
+
+        for(int col = 0; col < numCols; col++){
+            for(int row = 0; row < numRows; row++) {
+                data[row][col] += o.data[row][col];
+            }
+        }
+    }
+
+    public void mult(Object o) {
+        if(o instanceof Double) {
+            Double od = (Double) o;
+            multScalar(od);
+        }else if(o instanceof Integer) {
+            Integer oi = (Integer) o;
+            multScalar(oi);
+        }else if(o instanceof Float) {
+            Float of = (Float) o;
+            multScalar(of);
+        }else if(o instanceof Matrix) {
+            Matrix om = (Matrix) o;
+            multMatrix(om);
+        }
+    }
+
+    private void multScalar(double o) {
+        for(int col = 0; col < numCols; col++){
+            for(int row = 0; row < numRows; row++) {
+                data[row][col] *= o;
+            }
+        }
+    }
+
+    private void multMatrix(Matrix o) {
+        assert validMult(this, o);
+
+        int length = this.numRows;
+
+        Double[][] newData = new Double[o.numCols][this.numRows];
+
+        for(int row = 0; row < length; row++){
+            for(int col = 0; col < length; col++) {
+                newData[row][col] = dot(getRowAtIndex(row), o.data[col]);
+            }
+        }
+
+        this.data = newData;
+        this.numCols = data.length;
+        this.numRows = data[0].length;
+    }
+
+    private Double[] getRowAtIndex(int idx) {
+
+        Double[] row = new Double[numCols];
+
+        for(int i = 0; i < numCols; i++) {
+            row[i] = data[i][idx];
+        }
+
+        return row;
+    }
+
+    public static double dot(Double[] v1, Double[] v2) {
+        assert v1.length == v2.length;
+
+        double sum = 0;
+        for(int i = 0; i < v1.length; i++) {
+            sum += v1[i] * v2[i];
+        }
+
+        return sum;
+    }
+
+    public static boolean validMult(Matrix m1, Matrix m2) {
+        return m1.numCols == m2.numRows;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+
+        for(int col = 0; col < numCols; col++) {
+            for (int row = 0; row < numRows; row++) {
+                builder.append(String.format("%5.2f", data[col][row])).append(" ");
+            }
+            builder.append("\n");
+        }
+
+        builder.deleteCharAt(builder.lastIndexOf("\n"));
+        return builder.toString();
+    }
+
+}
